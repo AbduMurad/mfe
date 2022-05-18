9d6cdba53bb0978b36998b68597a7348046ae6d8
AbduMurad <abadeem@gmail.com>
2022-05-18 17:51:05 +0200

    initial commit

---
+++ packages/marketing/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { stylesProvider } from '@material-ui/core/styles';
+
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
+
+const App = () => {
+  return (
+    <div>
+      <stylesProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </stylesProvider>
+    </div>
+  );
+};
+
+export default App;
