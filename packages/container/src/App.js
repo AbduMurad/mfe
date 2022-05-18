59791f745441a2dfd840e4d9651f472904959618
AbduMurad <abadeem@gmail.com>
2022-05-18 19:51:32 +0200

    updated text in container/src/app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
   return (
     <div>
-      <h1>Hey, Welcome to My App</h1>
+      <h1>Welcome</h1>
       <hr />
       <MarketingApp />
     </div>
