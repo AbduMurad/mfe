4a6282c4729bbc34e81f62a6f8293376b4f6e790
AbduMurad <abadeem@gmail.com>
2022-05-19 15:49:48 +0200

    fix issue

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,7 +16,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
-        marketing: `marketing@https://${domain}/marketing/latest/remoteEntry.js`,
+        marketing: `marketing@${process.env.PRODUCTION_DOMAIN}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
