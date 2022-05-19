216cef6c1e7ad827ba5dcd6bee41cc668e2a3cf1
AbduMurad <abadeem@gmail.com>
2022-05-19 16:50:09 +0200

    added header

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { stylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 const App = () => {
   return (
     <div>
-      <stylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
         </BrowserRouter>
-      </stylesProvider>
+      </StylesProvider>
     </div>
   );
 };
